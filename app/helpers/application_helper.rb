a94493c942eba27ad2459d5ee754a6580127a5ba
Shaiane Boesing <shaianeboesingrb@gmail.com>
2022-09-15 14:49:14 -0300

    :sparkles: feat: define application customized helpers

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,5 @@
+module ApplicationHelper
+    def date_br(date_us)
+        date_us.strftime("%d/%m/%Y")
+    end
+end
---
+++ app/helpers/tasks_helper.rb
@@ -0,0 +1,16 @@
+module TasksHelper
+    def filter_helper(filter_type)
+        case filter_type
+        when "end"
+            @tasks = Task.order_by_end_soon
+        when "init"
+            @tasks = Task.order_by_init_soon
+        when "on"
+            @tasks = Task.filter_by_status_on
+        when "off"
+            @tasks = Task.filter_by_status_off
+        else
+            @tasks = Task.all
+        end
+    end
+end
