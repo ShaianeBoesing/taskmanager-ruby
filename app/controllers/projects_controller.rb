2744432c36132cd43a241006b8b544086b0cf205
Shaiane Boesing <shaianeboesingrb@gmail.com>
2022-09-15 16:16:21 -0300

    :sparkles: feat: paginates projects collection

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
 
   # GET /projects or /projects.json
   def index
-    @projects = Project.all
+    @projects = Project.page(params[:page]).per(10)
   end
 
   # GET /projects/1 or /projects/1.json
