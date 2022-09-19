a19a93eb32ca7e6317e2f6904090213d6b299a68
Shaiane Boesing <shaianeboesingrb@gmail.com>
2022-09-18 21:24:30 -0300

    :sparkles: feat: returns completed percentage of project

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -12,5 +12,18 @@ module ProjectsHelper
         return @projects.page(params[:page]).per(10).asc
     end
     
+    def calculate_completed(project)
+        completed = 0.0
+        done = project.tasks.where(:status => :true).count.to_f
+        total = project.tasks.count.to_f
+        if total > 0
+            completed = (done / total)* 100
+        end
+        completed
+    end
 
+    def percentual_format(value, decimal_houses)
+        value.round(decimal_houses).to_s + "%"
+    end
+    
 end
